296f1aff2a623039a57cdd41138d799d79cf054a
Стасян <stas.baranovfm@gmail.com>
2023-08-10 00:09:58 +0300

    3-rd save

--- HW3/assets/scripts/script.js
+++ HW3/assets/scripts/script.js
@@ -1,20 +1,5 @@
-function clickColor() {
-	var d
-	d = document.getElementById('tagP');
-
-	if (d.style.color='red') {
-		d.style.color='black';
-	}
-
-	if (d.style.color='black') {
-		d.style.color='red';
-	}
-
-	console.log(d.style.color);
+for (var p of document.getElementsByTagName('p')) {
+    p.onclick = function () {
+        this.style.color = this.style.color === 'red' ? 'black' : 'red';
+    }
 }
-
-// for (var p of document.getElementsByTagName('p')) {
-//     p.onclick = function () {
-//         this.style.color = this.style.color === 'red' ? 'black' : 'red';
-//     }
-// }
